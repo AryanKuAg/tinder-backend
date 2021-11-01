24e8fbc92532ce429dbc0bdcd93f74be2d785eeb
Aryan Agrawal <alemantrixaryan@gmail.com>
2021-11-01 20:38:53 +0530

    connected to the database

--- server.js
+++ server.js
@@ -1,29 +1,53 @@
-import express from 'express'
-import mongoose from 'mongoose'
+import express from "express";
+import mongoose from "mongoose";
+
+import Cards from "./dbCards.js";
 
 // App config
-const app = express()
+const app = express();
 const port = process.env.PORT || 7000;
-const connectionUrl = 'mongodb+srv://admin:QXJtHslccjwuLJp3@cluster0.c9hse.mongodb.net/tinderdb?retryWrites=true&w=majority'
+const connectionUrl =
+  "mongodb+srv://admin:QXJtHslccjwuLJp3@cluster0.c9hse.mongodb.net/tinderdb?retryWrites=true&w=majority";
 
+app.use(express.json())
 // middleware
 
 // db configs
 
 mongoose.connect(connectionUrl, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-})
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //api endpoints
-app.get('/', (req, res) => {
-    res.status(200).send('Works fine')
-})
+app.get("/", (req, res) => {
+  res.status(200).send("Works fine");
+});
 
+app.post("/tinder/cards", (req, res) => {
+  const dbCard = req.body;
+  console.log(dbCard)
+  Cards.create(dbCard, (err, data) => {
+    if (err) {
+      res.send(400).send("There is something wrong with your data");
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
+
+app.get("/tinder/cards", (req, res) => {
+  Cards.find({}, (err, data) => {
+    if (err) {
+      res.send(400).send("There is something wrong with your data");
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
 // listner
-app.listen(port, ()=> {
-    console.log('The app is up and running on port', port)
-})
+app.listen(port, () => {
+  console.log("The app is up and running on port", port);
+});
 
 // name - admin & password - QXJtHslccjwuLJp3
